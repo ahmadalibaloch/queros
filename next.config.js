fc1765ad87e0973b559cd4b78d1ac6de80607122
Ahmad Ali <ahmadalibaloch@gmail.com>
2022-06-12 20:19:23 +0500

    feat: initial comit with tailwind , nextjs in ts

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    dirs: ['src'],
+  },
+  images: {
+    domains: ['s3.amazonaws.com'],
+  }
 }
 
 module.exports = nextConfig
---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import Page from '@/components/Page';
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+  return <Page>Welcome To Qureos</Page>;
+};
+
+export default Home;
